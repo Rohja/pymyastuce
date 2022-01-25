05afbb9d794da3a9259444da5a55af85cbd7dd11
Paul Rohja LESELLIER <rohja@rohja.com>
2022-01-25 19:14:29 +0100

    Bump version in order to release on PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pymyastuce",
-    version="0.0.1",
+    version="0.0.2",
     author="Paul Rohja LESELLIER",
     author_email="rohja@rohja.com",
     description="A small package to fetch next bus/metro/tram at a station of the MyAstuce network in Rouen, France.",
